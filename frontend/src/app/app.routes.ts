fada8ec374a0f0716207901f4ff6dba0f039fe02
xilte pin <xiltepin@gmail.com>
2026-01-22 11:16:34 +0900

    added landing page, implementing drools

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RiskAssessmentComponent } from './risk-assessment/risk-assessment';
+import { LandingPageComponent } from './landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+  {
+    path: 'RiskAssessment',
+    component: RiskAssessmentComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ frontend/src/app/landing-page/landing-page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [
+    RouterLink,
+    MatCardModule,
+    MatButtonModule
+  ],
+  templateUrl: './landing-page.html',
+  styleUrls: ['./landing-page.scss']
+})
+export class LandingPageComponent {
+  title = 'Auto Insurance Risk Assessment System';
+}
